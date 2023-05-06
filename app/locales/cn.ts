2b634d245cb93cd3aa650be15ce16e3da594551d
AlexYuf <113398791+AlexYuf@users.noreply.github.com>
2023-05-06 12:31:59 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -3,7 +3,7 @@ import { SubmitKey } from "../store/app";
 const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
-    Unauthorized: "想用的同学看下微信号Alex_Yuf的朋友圈吧",
+    Unauthorized: "用一天五元，不限次数，想用的同学加下微信号Alex_Yuf，看他的朋友圈吧",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
